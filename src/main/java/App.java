9c1194398b3b5883794429d9ce6ab7f112d8665d
Mateusz Czekaj <mateusz.czek332@gmail.com>
2022-04-15 19:26:09 +0200

    delete i update
    Na podstronie cars można teraz usuwać i zmieniać właściwości samochodów

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,8 +3,12 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import spark.Request;
 import spark.Response;
+
+import java.awt.desktop.SystemEventListener;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Objects;
+
 import static spark.Spark.post;
 import static spark.Spark.staticFiles;
 
@@ -15,8 +19,8 @@ class App {
         staticFiles.location("/public");
         post("/add", (req, res) -> addFunction(req, res));
         post("/json", (req, res) -> daneFunction(req, res));
-//        post("/delete",(req, res) -> addFunction(req, res));
-//        post("/update",(req, res) -> addFunction(req, res));
+        post("/delete",(req, res) -> delFunction(req, res));
+        post("/update",(req, res) -> updateFunction(req, res));
     }
 
     static String addFunction(Request req, Response res) {
@@ -40,4 +44,28 @@ class App {
         return gson.toJson(cars, listType);
     }
 
+    static String delFunction(Request req, Response res) {
+        for (Car el: cars) {
+            if(Objects.equals(req.body().substring(1, 37), el.getUuid().toString())) {
+                cars.remove(el);
+                break;
+            }
+        }
+        return req.body();
+    }
+
+    static String updateFunction(Request req, Response res) {
+        Gson gson = new Gson();
+        Car updateCar = gson.fromJson(req.body(), Car.class);
+        System.out.println(updateCar);
+        for (Car el: cars) {
+            if( Objects.equals( el.getUuid(), updateCar.getUuid() )){
+                System.out.println(el);
+                el.setModel(updateCar.getModel());
+                el.setYear(updateCar.getYear());
+            }
+        }
+        return req.body();
+    }
+    
 }
--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -10,7 +10,8 @@ public class Car {
     private int year;
     private String color;
 
-    public Car(String model, ArrayList<Airbag> airbags, int year, String color) {
+    public Car(UUID uuid, String model, ArrayList<Airbag> airbags, int year, String color) {
+        this.uuid = uuid;
         this.model = model;
         this.airbags = airbags;
         this.year = year;
@@ -37,7 +38,27 @@ public class Car {
         this.uuid = uuid;
     }
 
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public UUID getUuid() {
+        return uuid;
     }
 }
